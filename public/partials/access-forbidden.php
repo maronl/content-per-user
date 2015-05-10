d1f3fc0f9682d1f6504fb04f34aeeb5170cb65af
Luca Maroni <lcmaroni77@gmail.com>
2015-05-10 23:24:11 +0200

    modificato il testo di default per il messaggio accesso riservato in inglese

--- public/partials/access-forbidden.php
+++ public/partials/access-forbidden.php
@@ -1,4 +1,4 @@
 <div class="jumbotron">
-    <h1><?php _e('Accesso Riservato', 'content-per-user'); ?></h1>
+    <h1><?php _e('Restricted Access', 'content-per-user'); ?></h1>
     <p><?php _e('To see the full article contact the administrator of the website', 'content-per-user'); ?></p>
 </div>
